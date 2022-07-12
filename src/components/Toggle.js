3524b94963d4aceff22d449bd29cccf8f83a9bbc
Asma <as1588ma@gmail.com>
2022-07-11 21:56:08 -0400

    complete homework

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+  const color = isOn ? "red" : "white";
+  return (
+    <button style={{ color: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
+
+//line 9: first set of {} is value of the prop.(it could be boolean, string, etc)
+//and the second set {} is an object here
